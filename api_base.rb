7888e1cc34423bd7f967b9df4811a6ca8762cabf
Hampus Nordin <nordin.hampus@gmail.com>
2015-09-21 23:08:38 +0200

    Added basic auth to API with Rack::Auth::Basic

--- api_base.rb
+++ api_base.rb
@@ -8,6 +8,16 @@ module Hampusn
   module MessageCache
     class API_Base < Grape::API
 
+      use Rack::Auth::Basic, "Protected Area" do |user, pass|
+        
+        # TODO: Match against database instead 
+        #       of these hardcoded values
+        user2 = 'devuser'
+        pass2 = 'devpass' 
+
+        user == user2 && pass == pass2
+      end
+
       mount Hampusn::MessageCache::API::Messages
 
     end
