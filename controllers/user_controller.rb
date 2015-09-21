bee28818ecb9e11b77d14c52bec1720d9fab8fa5
Hampus Nordin <nordin.hampus@gmail.com>
2015-09-21 23:00:01 +0200

    Created empty User Controller

---
+++ controllers/user_controller.rb
@@ -0,0 +1,17 @@
+# User Controller
+
+require 'sinatra/json'
+
+include Hampusn::MessageCache::Models
+
+module Hampusn
+  module MessageCache
+    module Controllers
+      class UserController < Sinatra::Base
+
+        # ...
+
+      end
+    end
+  end
+end
