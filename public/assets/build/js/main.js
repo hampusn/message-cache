e91490e913e7a9aa16728be3222e3d7514cafa18
Hampus Nordin <nordin.hampus@gmail.com>
2015-10-25 17:15:38 +0100

    Added skeleton main.js

---
+++ public/assets/build/js/main.js
@@ -0,0 +1,9 @@
+/*!
+ * @file
+ * @group hampusn-message-cache
+ * @author Hampus Nordin <nordin.hampus@gmail.com>
+ * 
+ * Version: 0.0.0
+ * Latest built: Sun Oct 25 2015 17:14:42 GMT+0100 (CET)
+ */
+!function(n,t,i,u){n(function(){})}(jQuery,this,this.document);
---
+++ public/assets/src/js/main.js
@@ -0,0 +1,17 @@
+/*!
+ * @file
+ * @group hampusn-message-cache
+ * @author Hampus Nordin <nordin.hampus@gmail.com>
+ * 
+ * Version: {{version}}
+ * Latest built: {{timestamp}}
+ */
+
+(function ($, window, document, undefined) {
+
+  // Document Ready
+  $(function () {
+
+  });
+
+})(jQuery, this, this.document);
