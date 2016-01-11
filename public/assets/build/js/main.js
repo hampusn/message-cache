f981d560a218b2a059647fea3226cc8b5ecd8765
Hampus Nordin <nordin.hampus@gmail.com>
2016-01-11 23:16:39 +0100

    Added basic styling for flash messages

--- public/assets/build/js/main.js
+++ public/assets/build/js/main.js
@@ -4,6 +4,6 @@
  * @author Hampus Nordin <nordin.hampus@gmail.com>
  * 
  * Version: 0.0.0
- * Latest built: Mon Jan 11 2016 22:42:10 GMT+0100 (CET)
+ * Latest built: Mon Jan 11 2016 23:04:56 GMT+0100 (CET)
  */
 !function(n,t,i,u){n(function(){})}(jQuery,this,this.document);
