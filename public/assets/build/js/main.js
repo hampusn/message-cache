c4a23a951a938f615e86093ed2296483cfc355ee
Hampus Nordin <nordin.hampus@gmail.com>
2016-01-11 18:50:05 +0100

    Added link to site title

--- public/assets/build/js/main.js
+++ public/assets/build/js/main.js
@@ -4,6 +4,6 @@
  * @author Hampus Nordin <nordin.hampus@gmail.com>
  * 
  * Version: 0.0.0
- * Latest built: Mon Jan 04 2016 23:56:29 GMT+0100 (CET)
+ * Latest built: Mon Jan 11 2016 18:37:41 GMT+0100 (CET)
  */
 !function(n,t,i,u){n(function(){})}(jQuery,this,this.document);
