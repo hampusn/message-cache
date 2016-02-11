801639e9fe9b93d9557df35888fbe059dc8b5bad
Hampus Nordin <nordin.hampus@gmail.com>
2016-02-11 22:16:11 +0100

    Added better validation of success_code param

--- api/middlewares/custom_status_code.rb
+++ api/middlewares/custom_status_code.rb
@@ -18,8 +18,12 @@ module Hampusn
             # env["api.endpoint"]
 
             if query_params.has_key? "success_code"
-              if status >= 200 && status < 300
-                status = query_params["success_code"]
+              success_code          = query_params["success_code"].to_i
+              success_code_is_valid = success_code >= 100 && success_code < 600
+              status_is_success     = status >= 200 && status < 300
+
+              if success_code_is_valid && status_is_success
+                status = success_code
               end
             end
 
